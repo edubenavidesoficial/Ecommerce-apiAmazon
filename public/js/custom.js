109c82e48dadaa6bf4642d88f6d296f08fa52380
henvisi-1994 <henvisi_1994@hotmail.com>
2023-11-26 22:42:44 -0500

    feat: :hammer: Categorias
    Se agrego listado de categorias desde listado

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,6 +3,13 @@ var productos = [
     { name: 'Producto2', precio: 15, imagen: 'https://picsum.photos/500/500' },
     { name: 'Producto3', precio: 30, imagen: 'https://picsum.photos/500/500' }
 ];
+var categorias =[
+    { name: 'Categoria 1', imagen: 'uploads/images/21-10-2023/a.jpg' },
+    { name: 'Categoria 2', imagen: 'uploads/images/21-10-2023/a.jpg' },
+    { name: 'Categoria 3', imagen: 'uploads/images/21-10-2023/a.jpg' },
+    { name: 'Categoria 4', imagen: 'uploads/images/21-10-2023/a.jpg' },
+    { name: 'Categoria 5', imagen: 'uploads/images/21-10-2023/a.jpg' },
+]
 
 // Función para agregar productos al contenedor
 function agregarProductos(data) {
@@ -25,8 +32,25 @@ function agregarProductos(data) {
         productContainer.append(productCard);
     });
 }
+function agregarCategorias(data) {
+    var categoryContainer = $('#category-container');
+
+    data.forEach(function(categoria) {
+        // Crea un nuevo elemento de categoría con los datos del arreglo
+        var categoryCard = $('<div class="card">');
+        var img = $('<img>').attr('src', categoria.imagen).attr('alt', 'Categoría');
+        var categoryName = $('<div class="category-name">').text(categoria.name);
+
+        // Construye la estructura de la categoría
+        categoryCard.append(img, categoryName);
+
+        // Agrega la categoría al contenedor
+        categoryContainer.append(categoryCard);
+    });
+}
 
 // Llama a la función para agregar productos cuando el documento esté listo
 $(document).ready(function() {
+    agregarCategorias(categorias) 
     agregarProductos(productos);
 });
